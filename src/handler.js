4536eb0a11eacf39efbe3b83e493dd34bdba8863
Daniil Kulinkovich <dankulinkovich@gmail.com>
2023-02-01 15:26:09 +0300

    rewrite handler to event delegating

--- src/handler.js
+++ src/handler.js
@@ -3,25 +3,25 @@ import CalculatorService from './services/calculatorService';
 import operatorMapper from './mappers/operatorMapper';
 import RenderService from './services/renderService';
 
-const calculatorFunctions = document.querySelectorAll('.command');
-const calculatorDigits = document.querySelectorAll('.btn-number');
+const calculatorButtons = document.getElementById('calculator-buttons');
 
 const calculator = new CalculatorService();
 const renderService = new RenderService(calculator, '#calculator-input');
 
-calculatorFunctions.forEach(e => e.addEventListener('click', () => {
-  const operationType = e.getAttribute('value');
-  renderService.renderOperation(operationType);
-  const command = operatorMapper(operationType, calculator.value);
-  if (command) {
-    calculator.command = command;
+calculatorButtons.onclick = (e) => {
+  const target = e.target;
+  if (target.classList.contains('btn-number')) {
+    const digit = target.value;
+    renderService.renderDigits(digit);
+  } else if (target.classList.contains('btn-operation') || target.classList.contains('btn-function')) {
+    const operationType = target.value;
+    renderService.renderOperation(operationType);
+    const command = operatorMapper(operationType, calculator.value);
+    if (command) {
+      calculator.command = command;
+    }
   }
-}));
-
-calculatorDigits.forEach(e => e.addEventListener('click', () => {
-  const digit = e.getAttribute('value');
-  renderService.renderDigits(digit);
-}));
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   renderService.init();
